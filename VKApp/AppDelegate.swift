e0082c711818ff1e24472b06b508807106c11fd9
Anna Zelinskaia <annazelinskaia@mail.ru>
2020-10-08 13:36:58 +0300

    Поправила авторизацию

--- VKApp/AppDelegate.swift
+++ VKApp/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	
 	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-		VKSdk.processOpen(url, fromApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?)
+		VKSdk.processOpen(url, fromApplication: "com.zelinskaya.VKApp")
 		return true
 	}
 }
